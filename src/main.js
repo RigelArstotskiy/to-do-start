43f7e1480d0d5c857c3d61edd400aa17ab7d2fe9
Mykyta <nik.from.varga@gmail.com>
2025-07-25 14:14:11 +0300

    button script done and local.storage

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 console.log(10)
 
 const form = document.querySelector('.form')//get a DOM element
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', event => {//handler
     event.preventDefault();
 
-    const textCard = form.querySelector('input[name="text-to-save"');
+    const textCard = form.querySelector('input[name="text-to-save"]');
     const trimCard = textCard.value.trim();
+    
+
+    if (trimCard === '') {
+        iziToast.error({
+            message: 'Заполни поле',
+            position: 'topRight'
+        });
+        return;
+    }
+
+    textCard.value = '';//clear form
+
+    const cardObject = {
+    text: trimCard,
+    }
+    cards.push(cardObject);
+
+    localStorage.setItem('cards', JSON.stringify(cards));
 
     console.log(9, {trimCard});//testing, is button works as it suppose to
 })
+
+const saved = localStorage.getItem('cards');
+const cards = saved ? JSON.parse(saved) : [];
+console.log(cards);//testing array
+
