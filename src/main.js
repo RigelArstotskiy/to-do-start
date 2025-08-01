cbca625087223c2276e74003a0efa257c4cd09a4
Mykyta <nik.from.varga@gmail.com>
2025-08-01 14:09:43 +0300

    add d-n-d functional

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-console.log(10)
+import Sortable from 'sortablejs';
 
 const form = document.querySelector('.form')//get a DOM element
 const cardContainer = document.querySelector('.card-show');
@@ -24,21 +24,21 @@ form.addEventListener('submit', event => {//handler
 
     const cardObject = {
     text: trimCard,
+    id: crypto.randomUUID(),
     }
+
     cards.push(cardObject);
 
     localStorage.setItem('cards', JSON.stringify(cards));
 
-    renderCard(trimCard)
-
-    console.log(9, {trimCard});//testing, is button works as it suppose to
+    renderCard(cardObject)
 })
 
 
-function renderCard(text) {
+function renderCard(card) {
   const cardHTML = `
-    <div class="card">
-      <span>${text}</span>
+    <div class="card" id="card-${card.id}">
+      <span>${card.text}</span>
       <button class="delete-btn">Удалить</button>
     </div>
   `;
@@ -48,21 +48,38 @@ function renderCard(text) {
 const saved = localStorage.getItem('cards');
 let cards = saved ? JSON.parse(saved) : [];
 
-console.log(cards);//testing array
-
 cards.forEach(card => {
-    renderCard(card.text);
+    renderCard(card);
 });
 
 cardContainer.addEventListener('click', event => {
     if (event.target.classList.contains('delete-btn')){
     const cardElement = event.target.closest('.card');
-    const text = cardElement.querySelector('span').textContent;
+    
+    const idAttr = cardElement.id;
+    const id = idAttr.replace('card-', '');
 
-    cards = cards.filter(card => card.text !== text);
+    cards = cards.filter(card => card.id !== id);
 
     localStorage.setItem('cards', JSON.stringify(cards));
 
     cardElement.remove();
     }
 });
+
+const sortable = new Sortable(cardContainer, {
+  animation: 150,
+  swapThreshold: 0.55,
+  onEnd: function (e) {
+    updateCardsOrder(e.oldIndex, e.newIndex);
+  },
+});
+
+function updateCardsOrder (oldIndex, newIndex) {
+  if(oldIndex === newIndex) return;
+
+  const movedCard = cards.splice(oldIndex, 1)[0];
+  cards.splice(newIndex, 0, movedCard);
+
+  localStorage.setItem('cards', JSON.stringify(cards));
+}
